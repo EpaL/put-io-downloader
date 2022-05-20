3351f1ea97f179004d77cbccbfd3c2affb5710d3
Edward Lawford <elawford@gmail.com>
2022-05-20 12:03:24 -0400

    Able to retrieve a list of transfers from put.io

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,18 @@
 import { ActionPanel, Detail, List, Action } from "@raycast/api";
+import PutioAPI from '@putdotio/api-client'
 
 export default function Command() {
+
+  const putioAPI = new PutioAPI({ clientID: '5746' })
+  putioAPI.setToken("VGDZI7MZV4EHEVUQOMZN")
+  putioAPI.Account.Info()
+    .then(r => console.log('Fetched user info: ', r))
+    .catch(e => console.log('An error occurred while fetching user info: ', e))
+  
+  putioAPI.Transfers.Query(100, 100)
+    .then(t => console.log('Fetched transfers: ', t.data))
+    .catch(e => console.log('An error occurred while fetching transfers: ', e))
+
   return (
     <List>
       <List.Item
