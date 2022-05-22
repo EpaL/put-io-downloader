200f798c29375e70bb19a8de2192303a343cce43
Edward Lawford <elawford@gmail.com>
2022-05-22 14:52:53 -0400

    Implemented external file download commands

--- src/views/FileBrowser.tsx
+++ src/views/FileBrowser.tsx
@@ -1,19 +1,21 @@
-import { ActionPanel, showToast, Toast, Detail, List, Action, Icon, useNavigation } from "@raycast/api";
+import { ActionPanel, showToast, Toast, showHUD, Detail, List, Action, Icon, useNavigation } from "@raycast/api";
 import { useEffect, useState } from "react";
 import DownloadFile from "../components/DownloadFile";
 import FileDetails from "../components/FileDetails";
+import formatString from "../utils/formatString";
 import PutioAPI, { Transfer  } from '@putdotio/api-client'
 import { preferences } from "../preferences";
 
 function FileBrowser({parent_file_id}: {parent_file_id: number}) {
   // State vars and handlers
   const [file, setFile] = useState<IFile>();
+  const [fileUrl, setFileUrl] = useState<string>();
   const [files, setFiles] = useState<IFile[]>();
+  const [downloadType, setDownloadType] = useState<string>();
   const [isShowingDetail, setIsShowingDetail] = useState(false);  
   const [error, setError] = useState<Error>();
+  const { exec } = require("child_process");
   const { push } = useNavigation();
-
-  console.log("parent file id: ", parent_file_id);
 
   useEffect(() => {
     if (error) {
@@ -26,6 +28,8 @@ function FileBrowser({parent_file_id}: {parent_file_id: number}) {
     }
   }, [error]);
 
+  //
+  // Populate the list of files (if a folder), or information about the file (if a single file).
   useEffect(() => {
     // Init put.io API
     const putioAPI = new PutioAPI({ clientID: preferences.putioClientId })
@@ -39,6 +43,16 @@ function FileBrowser({parent_file_id}: {parent_file_id: number}) {
           // If the files array is empty, it means this is just a file, not a directory.
           // Set the file to the 'parent' object, which contains the file info.
           setFile(t.data.parent);
+          // Get the URL of the file
+          putioAPI.File.GetStorageURL(t.data.parent.id)
+            .then(t => {
+              console.log('File URL is: ', t.data.url); 
+              setFileUrl(t.data.url)
+            })
+            .catch(e => { 
+              console.log('An error occurred while fetching file URL: ', e)
+              setError(new Error("Error fetching file URL details. Check your Client ID and OAuth Token settings."))
+            });
         } else {
           // The files array isn't empty, which means this is a folder.
           // Set the files object to the 'files' array, which contains the list of files in the folder.
@@ -60,6 +74,32 @@ function FileBrowser({parent_file_id}: {parent_file_id: number}) {
       })
   }, [parent_file_id]);
 
+  //
+  // Handle initiating file downloads
+  useEffect(() => {
+    if (fileUrl !== undefined && downloadType !== undefined) {
+      var cmd = null
+      console.log("Downloading ", fileUrl);
+      if (downloadType == "TVSHOW") {
+        cmd = formatString(preferences.tvShowDownloadCommand, fileUrl);
+      } else if (downloadType == "MOVIE") {
+        cmd = formatString(preferences.movieDownloadCommand, fileUrl);
+      }
+      console.log("Executing command: ", cmd);
+      exec(cmd, (error, stdout, stderr) => {
+        if (error) {
+          console.log(`error: ${error.message}`);
+          return;
+        }
+        if (stderr) {
+          console.log(`stderr: ${stderr}`);
+          return;
+        }
+        console.log(`stdout: ${stdout}`);
+        showHUD("⬇️ Download started.");
+    });
+    }
+  }, [downloadType]);  
 
   // If neither files or file are populated yet, display an empty list with the loading animation.
   if (files === undefined && file === undefined) {
@@ -79,7 +119,6 @@ function FileBrowser({parent_file_id}: {parent_file_id: number}) {
       >
         { files && 
           Object.values(files).map(file => {
-  
           return (
             <List.Item
             key={`${file.id}`}
@@ -115,16 +154,25 @@ function FileBrowser({parent_file_id}: {parent_file_id: number}) {
           }
           actions={
             <ActionPanel title="File Actions">
+              {
+                fileUrl && (
+                  <Action.OpenInBrowser url={fileUrl} />              
+                )
+              }
               <Action
                 title={"Download TV Show"}
                 icon={Icon.Download}
-                onAction={() => push(<DownloadFile file={file} type="TVSHOW"/>)}
+                onAction={() => {
+                  setDownloadType("TVSHOW");
+                }}
                 />
               <Action
                 title="Download Movie"
                 icon={Icon.Download}
-                onAction={() => push(<DownloadFile file={file} type="MOVIE"/>)}
-              />
+                onAction={() => {
+                  setDownloadType("MOVIE");
+                }}             
+                />
             </ActionPanel>
           }
           >
@@ -132,7 +180,6 @@ function FileBrowser({parent_file_id}: {parent_file_id: number}) {
       );
     }
   }
-
 }
 
 function formatFileInfo(file: IFile): string
